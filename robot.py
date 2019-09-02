dfdec839be13afe0dec04d5158e7797631aa2b54
Janek <janzborowski99@gmail.com>
2019-09-02 19:43:46 +0200

    Fetchin instructions from http server

--- robot.py
+++ robot.py
@@ -1,9 +1,14 @@
 import sys
 import math
+import requests
+import time
 sys.path.insert(0, "./modules")
 
 from Braccio import Braccio
 from Position import Position
+
+instruction_url = "http://localhost:3000/"
+defualt_payload = ["robot"]
 
 stop = False
 
@@ -38,54 +43,73 @@ def calc_grab_pos(p1, p2, percent):
     new_angles = [ a - b for (a, b) in zip(p1.angles, differences)]
     return Position(*new_angles)
 
-def read_input():    
+def fetch_instruction():    
     global stop
     global robot
-
-    while not stop:
-        inp = input("> ")
-        if inp == "exit" or inp == "quit":
-            stop = True
-            robot.reset(speeds["s"], delay = 2)
-            robot.power_off()
+    r = None
+    try:
+        r = requests.post(instruction_url, json=defualt_payload, timeout=15)
+    except Exception as e:
+        print(e)
+        time.sleep(5)
+        return
+    
+    if r.status_code != 200:
+        return
+    instruction = r.json()
         
-        elif inp.startswith("point "):
-            robot.reset(speeds["s"])
-            [_ , x, y] = inp.split(" ")
-            
-            target_angle = get_rotation_angle(int(x), int(y))
-            
-            point_pos =  point_pos_default.copy().set(0, target_angle)
-            robot.move_to_position(point_pos, speeds["m"])
+    action = instruction["type"]
+    
+    if action is None:
+        return
+
+    x, y, cam_dist = instruction["coords"]
+
+    if action == "exit" or action == "quit":
+        stop = True
+        robot.reset(speeds["s"], delay = 2)
+        robot.power_off()
+    
+    elif action.startswith("point"):
+        robot.reset(speeds["s"])            
         
-        elif inp.startswith("grab "):
-            [_ , x, y] = inp.split(" ")
-            robot.reset(speeds["s"])
-            x, y = [int(x), int(y)]
-            distance = distance_from_origin(x,y) // 10
-            target_angle = get_rotation_angle(x, y)
-            pos = None
-            if distance >= close_range[0] and distance < close_range[1]:
-                p = (distance - close_range[0]) * 100/(close_range[1] - close_range[0])
-                pos = calc_grab_pos(grab_pos_close_start.copy(), grab_pos_close_end.copy(), p)
-                
+        target_angle = get_rotation_angle(int(x), int(y))            
+        point_pos =  point_pos_default.copy().set(0, target_angle)
+        robot.move_to_position(point_pos, speeds["m"])
+    
+    elif action.startswith("grab"):            
+        robot.reset(speeds["s"])
 
-            elif distance >= far_range[0] and distance <= far_range[1]:
-                p = (distance - far_range[0]) * 100 / (far_range[1] - far_range[0])
-                pos = calc_grab_pos(grab_pos_far_start.copy(), grab_pos_far_end.copy(), p)                
+        x, y = [int(x), int(y)]
+        distance = distance_from_origin(x,y) // 10
+        target_angle = get_rotation_angle(x, y)
+        pos = None
+        if distance >= close_range[0] and distance < close_range[1]:
+            p = (distance - close_range[0]) * 100/(close_range[1] - close_range[0])
+            pos = calc_grab_pos(grab_pos_close_start.copy(), grab_pos_close_end.copy(), p)
+            
 
-            pos.add(1, 20).set(0, target_angle)
-            robot.move_to_position(pos, speeds["s"], delay=1.5)
-            pos.add(1, -20)
-            robot.move_to_position(pos, speeds["s"])
-            robot.close_gripper(delay=1)
-            robot.reset(speeds["vs"])
+        elif distance >= far_range[0] and distance <= far_range[1]:
+            p = (distance - far_range[0]) * 100 / (far_range[1] - far_range[0])
+            pos = calc_grab_pos(grab_pos_far_start.copy(), grab_pos_far_end.copy(), p)                
+        else:
+            resp = requests.post(instruction_url + "error", json={"msg": "Error during performing the instruction", "instruction": instruction })            
+            time.sleep(3)
+            return
 
-        elif inp.startswith("reset"):
-            robot.reset(speeds["s"])
+        pos.add(1, 20).set(0, target_angle)
+        robot.move_to_position(pos, speeds["s"], delay=1.5)
+        pos.add(1, -20)
+        robot.move_to_position(pos, speeds["s"])
+        robot.close_gripper(delay=1)
+        robot.reset(speeds["vs"])
+
+    elif action.startswith("reset"):
+        robot.reset(speeds["s"])
 
 robot = Braccio(sys.argv[1] if len(sys.argv) > 1 else "COM1", start_pos)
 
 if __name__ == "__main__":
     robot.power_on()
-    read_input()
+    while not stop:
+        fetch_instruction()
